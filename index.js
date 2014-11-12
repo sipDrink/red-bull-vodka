49c4c36e4d1a0315cde29d91c523c4f1b49fe1b5
Hendrixer <willscottmoss@gmail.com>
2014-11-11 23:48:03 -0800

    configure index server file

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
+'use strict';
+
+var config = require('./config/env');
+var mongoose = require('mongoose');
 var http = require('http');
-var pubKey = process.env.PUBNUB_PUBLISH_KEY || 'demo';
-var sbKey = process.env.PUBNUB_SUBSCRIBE_KEY || 'demo';
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+mongoose.connect(config.mongo.uri, config.mongo.options);
+
+if (config.seedDB) { require('./config/seed'); }
+
+/* Init pubnub and register channels */
+require('./api/events')(require('./pb/pbinit'));
 
 
-var pb = require('pubnub').init({
-  publish_key: pubKey,
-  subscribe_key: sbKey
-});
+// var pb = require('pubnub').init({
+//   publish_key: pubKey,
+//   subscribe_key: sbKey
+// });
 
-setInterval(function() {
-  pb.publish({
-    channel: 'sip',
-    message: { code: 'theme streams' },
-    callback: function(e){console.log('pubped')},
-    error: function(e){console.log(e)}
-  });
-}, 5000);
+// setInterval(function() {
+//   pb.publish({
+//     channel: 'sip',
+//     message: { code: 'theme streams' },
+//     callback: function(e){console.log('pubped')},
+//     error: function(e){console.log(e)}
+//   });
+// }, 5000);
 
 
 http.createServer(function(req, res) {
