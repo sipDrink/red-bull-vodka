85e90ff8dfc759cdaf22b2e2bcc63911c73e926d
Hendrixer <willscottmoss@gmail.com>
2014-11-15 23:06:25 -0800

    update pb

--- api/events.js
+++ api/events.js
@@ -3,13 +3,29 @@
 
 module.exports = function(pb) {
 
-  setInterval(function() {
-    pb.publish({
-      channel: 'sip',
-      message: { code: 'theme streams' },
-      callback: function(e){console.log('pubped')},
-      error: function(e){console.log(e)}
-    });
-  }, 5000);
+  // setInterval(function() {
+  //   pb.publish({
+  //     channel: 'sip',
+  //     message: { code: 'theme streams' },
+  //     callback: function(e){console.log('pubped')},
+  //     error: function(e){console.log(e)}
+  //   });
+  // }, 5000);
 
+  pb.subscribe({
+    channel: 'global-grant',
+    message: function(data) {
+      console.log(data);
+      pb.publish({
+        channel: 'sip',
+        message: { message: 'yooo' },
+        error: function(e){
+          console.error('pub error', e);
+        }
+      });
+    },
+    error: function(e){
+      console.error(e);
+    }
+  });
 };
