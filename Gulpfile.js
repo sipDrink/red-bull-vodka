2af0c33e186714e11efebed37fca7638475d9be1
Hendrixer <willscottmoss@gmail.com>
2014-11-28 14:26:59 -0800

    add gulp task to generate docs

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var _ = require('lodash');
 var jsDoc = require('gulp-jsdoc-to-markdown');
+var concat = require('gulp-concat');
 var localVars;
 
 // try to load up local vars
@@ -20,7 +21,11 @@ gulp.task('set', function() {
 
 gulp.task('document', function() {
   return gulp.src(['index.js','api/**/*.js', 'config/**/*.js'])
+    .pipe(concat('doc.md'))
     .pipe(jsDoc())
+    .on('error', function(err) {
+      console.log(err.message);
+    })
     .pipe(gulp.dest('documentation/'));
 });
 
---
+++ documentation/doc.md
@@ -0,0 +1,15 @@
+<a name="$Dispatcher"></a>
+#class: $Dispatcher
+**Members**
+
+* [class: $Dispatcher](#$Dispatcher)
+  * [new $Dispatcher(PubNub)](#new_$Dispatcher)
+
+<a name="new_$Dispatcher"></a>
+##new $Dispatcher(PubNub)
+Abstraction of the pubnub object used in the server.
+
+**Params**
+
+- PubNub `object` - if supplied used to create the pubnub abstraction.  
+
