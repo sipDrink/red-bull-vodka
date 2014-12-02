388f1053b1abaf2f6194b1faf509e14fb86eed5b
Hendrixer <willscottmoss@gmail.com>
2014-12-01 21:59:29 -0800

    add mock exports for bartenders

--- api/events.js
+++ api/events.js
@@ -3,6 +3,7 @@
 module.exports = function() {
   // For each resource, create and new $Dispatcher instance
   _.forEach($channels, function(resource) {
+    console.log(resource);
     require('./' + resource)(new $Dispatcher());
   });
 };
