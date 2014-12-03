8729fb73258733ebebd5a58807d4384bd4b2840d
Hendrixer <willscottmoss@gmail.com>
2014-12-02 20:55:34 -0800

    add association to bar from DrinkTypeModel

--- api/drinkTypes/drinkTypeModel.js
+++ api/drinkTypes/drinkTypeModel.js
@@ -2,17 +2,22 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var DrinkTypeSchema = new Schema({
+
   name: {
     type: String,
     required: true
   },
+
   bar: {
-    type: Schema.Obj
+    type: Schema.ObjectId,
+    ref: 'ref'
   },
+
   brands: [{
     name: String,
     price: Number
   }]
+
 });
 
 module.exports = mongoose.model('drinkType', DrinkTypeSchema);
