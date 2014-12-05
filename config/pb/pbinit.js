964063813238b981753ff767f5794e445fd7e93a
Hendrixer <willscottmoss@gmail.com>
2014-12-04 21:38:38 -0800

    hmmm

--- config/pb/pbinit.js
+++ config/pb/pbinit.js
@@ -1,48 +1,49 @@
 'use strict';
 
 function $Dispatcher(PubNub){
-  var pb = PubNub || require('pubnub').init($config.secrets.pb);
-  var that = this;
-  this.pub = function(message, channel) {
-    message.from = $config.alias;
-    message.to = 'mobile';
-    pb.publish({
-      channel: channel,
-      message: message,
-      callback: function(){
-        $log('message sent to ' + message.to);
-      },
-      error: function(error) {
-        $handleError(error, "PUB Error");
-        that.retryQueue.push({message: message, channel: channel});
-      }
-    });
-  };
+  this.pb = PubNub || require('pubnub').init($config.secrets.pb);
+  this.retryQueue = [];
 
-  this.sub = function(channel, cb) {
-    pb.subscribe({
-      channel: channel,
-      callback: cb,
-      error: function(e) {
-        $handleError(e)
-      }
-    });
-  };
-
-  this.grant = function(configs) {
-    pb.grant(configs);
-  };
-
-  this.retryQueue = [],
   this.repub = function() {
     var what = this.retryQueue.pop();
     if (what) {
       setTimeout(function() {
-        this.pub()
+        this.pub();
       }.bind(this), 200);
     }
   };
 }
 
+$Dispatcher.prototype.pub = function(message, channel) {
+  var that = this;
+  message.from = $config.alias;
+  message.to = 'mobile';
+  this.pb.publish({
+    channel: channel,
+    message: message,
+    callback: function(){
+      $log('message sent to ' + message.to);
+    },
+    error: function(error) {
+      $handleError(error, "PUB Error");
+      that.retryQueue.push({message: message, channel: channel});
+      $log('retry', that.retryQueue.length);
+    }
+  });
+};
+
+$Dispatcher.prototype.sub = function(channel, cb) {
+  this.pb.subscribe({
+    channel: channel,
+    callback: cb,
+    error: function(e) {
+      $handleError(e);
+    }
+  });
+};
+
+$Dispatcher.prototype.gant = function(configs) {
+  this.pb.grant(configs);
+};
 /** @global */
 global.$Dispatcher = $Dispatcher;
