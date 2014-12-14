8783a94b206e370a12b085d534277025db27850e
Hendrixer <willscottmoss@gmail.com>
2014-12-13 20:47:55 -0800

    fixed the error

--- api/orders/orderActions.js
+++ api/orders/orderActions.js
@@ -5,12 +5,11 @@ var actions = require('../createActions')(Order);
 actions.order = function(params, $dispatcher, res){
   $log('params', params);
   var merch = params.merch || 'CU5EeFyjWXJjMeHduZmDb9Ac';
-  var order = params.order;
-  order.paidFor = false;
+  params.paidFor = false;
 
   var createOrder = $q.nbind(Order.create, Order);
 
-  createOrder(order).then(function(unPaidForOrder){
+  createOrder(params).then(function(unPaidForOrder){
     var orderContent = {
       description: 'Order # ' + unPaidForOrder._id
     };
