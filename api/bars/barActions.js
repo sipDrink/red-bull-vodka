8a0a8c18d016ace3ff16a3d72de33f480d57c696
sunny-g <sunny.gonna@gmail.com>
2014-12-19 15:33:57 -0800

    add bar actions to grant bars access to their channels

--- api/bars/barActions.js
+++ api/bars/barActions.js
@@ -1,3 +1,37 @@
 'use strict';
 var actions = require('../createActions')(Bar);
+
+actions.register = function(auth_key, $dispatcher) {
+  $log('registering bar');
+  /*
+    Channels to grant bars publish access to:
+      bars
+      orders
+      categories
+      drinkTypes
+   */
+//  var channelsToGrant = $channels.slice();
+
+  var channelsToGrant = _.remove($channels, function(channel) {
+    return !/drinks/g.test(channel);
+  });
+
+  // Grant bar access to their own private channel
+  channelsToGrant.push(auth_key);
+
+  // Grant bar access to their relevant channels
+  _.forEach(channelsToGrant, function(channel) {
+    $dispatcher.grant({
+      channel: channel,
+      read: false,
+      write: true,
+      ttl: 0,
+      auth_key: auth_key,
+      callback: function() {
+        $log(channel + 'grant to bar successful');
+      }
+    });
+  });
+};
+
 module.exports = actions;
